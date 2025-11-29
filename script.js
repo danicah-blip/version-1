3bb8bea2cbf619ad9bce87e931bdeb9a7f911854
williamGats <william.gatdula@olivarezcollegetagaytay.edu.ph>
2025-11-29 21:17:47 +0800

    FinalDeploy

--- script.js
+++ script.js
@@ -47,3 +47,11 @@ const button = document.getElementById('to-munebtn');
 button.addEventListener('click', () =>{
     window.location.href = 'menu-side.html';
 })
+
+/*Map button*/
+const mapbutton = document.getElementById('mapb');
+
+mapbutton.addEventListener('click', () =>{
+
+    window.location.href='https://www.google.com/maps/place/Bogchi+Kusina/@14.1244082,120.9028387,388m/data=!3m1!1e3!4m6!3m5!1s0x33bd9d003ee95127:0x5d5045670da7978!8m2!3d14.1242935!4d120.9034258!16s%2Fg%2F11y371m1dq!5m1!1e4?entry=ttu&g_ep=EgoyMDI1MTEyMy4xIKXMDSoASAFQAw%3D%3D';
+})  
