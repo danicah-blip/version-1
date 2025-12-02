8511257b41fb6674184e506fbeba553af049c958
williamGats <william.gatdula@olivarezcollegetagaytay.edu.ph>
2025-12-02 21:49:25 +0800

    Documentations

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+/*Logo header button logic*/
 document.querySelector(".logo img").addEventListener("click", () => {
     window.scrollTo({
         top: 0,
         behavior: "smooth"
     });
 });
+/*Logo header button logic*/
 
+
+/*Smooth scroll logic*/
 const navLinks = document.querySelectorAll(".nav a");
 const headerHeight = document.querySelector(".header").offsetHeight;
 
@@ -20,38 +24,23 @@ navLinks.forEach(link => {
         });
     });
 });
+/*Smooth scroll logic*/
 
-const sections = document.querySelectorAll("section");
 
-window.addEventListener("scroll", () => {
-    let current = "";
-    sections.forEach(section => {
-        const sectionTop = section.offsetTop - headerHeight - 20;
-
-        if (window.pageYOffset >= sectionTop) {
-            current = section.getAttribute("id");
-        }
-    });
-
-    navLinks.forEach(link => {
-        link.classList.remove("active");
-
-        if (link.getAttribute("href") === "#" + current) {
-            link.classList.add("active");
-        }
-    });
-});
-
+/*To menu button logic*/
 const button = document.getElementById('to-munebtn');
 
 button.addEventListener('click', () =>{
     window.location.href = 'menu-side.html';
 })
+/*To menu button logic*/
 
-/*Map button*/
+
+/*Map button logic*/
 const mapbutton = document.getElementById('mapb');
 
 mapbutton.addEventListener('click', () =>{
 
     window.location.href='https://www.google.com/maps/place/Bogchi+Kusina/@14.1244082,120.9028387,388m/data=!3m1!1e3!4m6!3m5!1s0x33bd9d003ee95127:0x5d5045670da7978!8m2!3d14.1242935!4d120.9034258!16s%2Fg%2F11y371m1dq!5m1!1e4?entry=ttu&g_ep=EgoyMDI1MTEyMy4xIKXMDSoASAFQAw%3D%3D';
 })  
+/*Map button logic*/
